d40dffd0d2e2b67e12bfb325b732a495bb073e64
Marc-Andre Giroux <mgiroux@netflix.com>
2024-03-08 11:53:11 -0500

    log error on internal error

--- apollo-router/src/axum_factory/axum_http_server_factory.rs
+++ apollo-router/src/axum_factory/axum_http_server_factory.rs
@@ -625,6 +625,8 @@ async fn handle_graphql(
                 return Elapsed::new().into_response();
             }
 
+            tracing::error!("Internal Error: {:?}", e);
+
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 "router service call failed",
