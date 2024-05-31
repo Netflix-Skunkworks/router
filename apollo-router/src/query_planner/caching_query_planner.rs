4e6f9f32bde044e3a3fa30a60d1bdb757510b7ba
Marc-Andre Giroux <mgiroux@netflix.com>
2024-05-31 12:48:43 -0400

    use std mutex for query analysis cache, use spawn_blocking for parsing, add metrics

--- apollo-router/src/query_planner/caching_query_planner.rs
+++ apollo-router/src/query_planner/caching_query_planner.rs
@@ -263,7 +263,11 @@ where
         } in all_cache_keys
         {
             let context = Context::new();
-            let doc = match query_analysis.parse_document(&query, operation.as_deref()) {
+
+            let doc = match query_analysis
+                .parse_document(&query, operation.as_deref())
+                .await
+            {
                 Ok(doc) => doc,
                 Err(_) => continue,
             };
@@ -301,7 +305,10 @@ where
                 })
                 .await;
             if entry.is_first() {
-                let doc = match query_analysis.parse_document(&query, operation.as_deref()) {
+                let doc = match query_analysis
+                    .parse_document(&query, operation.as_deref())
+                    .await
+                {
                     Ok(doc) => doc,
                     Err(error) => {
                         let e = Arc::new(QueryPlannerError::SpecError(error));
